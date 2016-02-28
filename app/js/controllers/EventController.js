3a64e0cc265dd72a5218a3c5a0115e73fc694b26
zeb aslam <xzeb.aslam@gmail.com>
2016-02-28 15:01:36 -0500

    Created necessary event details angular controller as well angular bindings based on controller. Also added image using ng-src attribute

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,13 +1,18 @@
 "use strict";
 
 eventsApp.controller('EventController',
-function EventController($scope)
-{
-    $scope.event = {
-       name: 'Angular Boot Camp',
-       date: '2/28/2016',
-       time: '12:43 AM'
-    }
+    function EventController($scope) {
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '2/28/2016',
+            time: '12:43 AM',
+            location: {
+                address: 'Zebs Apartment',
+                city: 'New York',
+                province: "NY"
+            },
+            imageUrl: 'img/angularjs-logo.png'
+        }
 
-}
+    }
 );
